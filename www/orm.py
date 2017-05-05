c4b7704a489c027438ad1fc1996b4451cb606214
JackZhang <jackzhang0817@qq.com>
2017-05-05 10:21:44 +0800

    增加服务文件

---
+++ www/orm.py
@@ -0,0 +1,16 @@
+@asyncio.coroutine
+def create_pool(loop,**kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = yield from aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset','utf-8'),
+        autocommit=kw.get('autocommit',True),
+        maxsize=kw.get('maxsize',10),
+        minsize=kw.get('minsize',1),
+        loop=loop
+    )
